960319e62eccd9e57d23faa27e9cfdd6f6f5c286
atul anand <37778673+atul161@users.noreply.github.com>
2018-10-13 08:28:26 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/felix/cordinatorlayoutapp/cordinatorlayoutapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/felix/cordinatorlayoutapp/cordinatorlayoutapp/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.felix.cordinatorlayoutapp.cordinatorlayoutapp", appContext.getPackageName());
+        //helllo how are you
     }
 }
